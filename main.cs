25ff0fea473de68246a244638234c41f9b2a1359
AlineShimoi <alineshimoi@yahoo.com.br>
2026-08-16 20:04:07 -0300

    fix/quantidade-de-alunos-para-cadastro

--- main.cs
+++ main.cs
@@ -31,7 +31,7 @@ class Program
         
         if(qtdNulos == 10)
         {
-            for(int i = 0; i < 2; i++)
+            for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Nome: ");
                 nomeAluno[i] = Console.ReadLine();
