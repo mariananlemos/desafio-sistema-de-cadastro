8a4da48fe7c635c917eb3ac2382b9180d1f600e1
luanaferreirasouza <luanaferreirasouza99@gmail.com>
2026-08-17 14:35:49 -0700

    Primeira versão do código no busca.cs

---
+++ busca.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Digite o nome do aluno:");
+string nomeBuscado = Console.ReadLine();
+bool encontrado = false;
+
+for (int i =0; i < nomeAluno.length(); i++)
+{
+    if (nomeBuscado == nomeAluno[i])
+    {
+        Console.WriteLine($"Nome:{nomeAluno[i]}");                Console.WriteLine($"Idade:{idadeAluno[i]}");
+        Console.WriteLine($"Nota 1:{nota1[i]}");
+        Console.WriteLine($"Nota 2:{nota2[i]}");
+        Console.WriteLine($"Média:{(nota1[i] + nota2[i])/2}");
+        encontrado = true;
+        break;
+    }
+
+}
+if (encontrado == false)
+{
+    Console.WriteLine("Aluno não encontrado!");
+}
