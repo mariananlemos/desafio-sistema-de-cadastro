9302128699ef750b123597ab8c627d1d571460cc
Daianne <daiannec@gmail.com>
2026-08-18 19:52:25 -0300

    Merge main aprovacao.cs case4 and case3

--- aprovacao.cs
+++ aprovacao.cs
@@ -1,30 +1,34 @@
 
-    // Método para listar alunos aprovados - Daianne
-using System;
+//     // Método para listar alunos aprovados - Daianne
+// using System;
 
-public class Aprovacao
-{
-    public static void ListarAprovados(
-        string[] nomeAluno,
-        double[] nota1,
-        double[] nota2)
-    {
-        Console.WriteLine("\n===== ALUNOS APROVADOS =====\n");
+// public class Aprovacao
+// {
+//     public static void ListarAprovados(
+//         string[] nomeAluno,
+//         double[] nota1,
+//         double[] nota2)
+//     {
+//         Console.WriteLine("\n===== ALUNOS APROVADOS =====\n");
 
-        int totalAprovados = 0;
+//         int totalAprovados = 0;
+//         double somamedias = 0;
 
-        for (int i = 0; i < nomeAluno.Length; i++)
-        {
-            double media = (nota1[i] + nota2[i]) / 2;
+//         for (int i = 0; i < nomeAluno.Length; i++)
+//         {
+//             double media = (nota1[i] + nota2[i]) / 2;
+//             somamedias += media; 
 
-            if (media >= 7)
-            {
-                Console.WriteLine($"{nomeAluno[i]} - Média {media:F1}");
-                totalAprovados++;
-            }
-        }
+//             if (media >= 7)
+//             {
+//                 Console.WriteLine($"{nomeAluno[i]} - Média {media:F1}");
+//                 totalAprovados++;
+//             }
+//         }
+//     double mediaturma = somamedias / nomeAluno.Length;
 
-        Console.WriteLine("---------------------------------------");
-        Console.WriteLine($"Total = {totalAprovados} aluno(s)");
-    }
-}
+//         Console.WriteLine("---------------------------------------");
+//         Console.WriteLine($"Total = {totalAprovados} aluno(s)");
+//         Console.WriteLine($"Média da turma = {mediaturma:F2}");
+//     }
+// }
--- obj/Debug/net10.0/desafio-sistema-de-cadastro.AssemblyInfo.cs
+++ obj/Debug/net10.0/desafio-sistema-de-cadastro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafio-sistema-de-cadastro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b42452ba698a7abad1c68472453590389a2d6e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b32ef62257d8539ebef2189cff1eb97f3b3f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafio-sistema-de-cadastro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafio-sistema-de-cadastro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
